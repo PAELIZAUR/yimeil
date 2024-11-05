18b615e744a043edb37a10ad74e754f9c9f8b2a8
paelizaur <paulelizaur20@gmail.com>
2024-11-05 20:18:58 -0300

    yimeil

--- src/main/java/edu/unsada/yimeil/Controller/CorreoController.java
+++ src/main/java/edu/unsada/yimeil/Controller/CorreoController.java
@@ -1,14 +1,22 @@
 package edu.unsada.yimeil.Controller;
 
+import edu.unsada.yimeil.models.Correo;
+import edu.unsada.yimeil.repository.CorreoRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/correo")
 
 public class CorreoController {
 
+        @Autowired
+        private CorreoRepo correoRepo;
         //  {crear 2 metodos
     //1- getAllMails
     //2- SendMails}
@@ -16,5 +24,13 @@ public class CorreoController {
         @GetMapping("/mostrardatos")
         public void mostrardatos(){
             System.out.println("hola mundo");
+        }
+        @GetMapping ("/getAllCorreo")
+        public ResponseEntity<?>GetAllCorreo(){
+
+
+            List<Correo> correos = (List<Correo>) correoRepo.findAll();
+            return ResponseEntity.ok(correos);
+
         }
 }
--- src/main/java/edu/unsada/yimeil/repository/CorreoRepo.java
+++ src/main/java/edu/unsada/yimeil/repository/CorreoRepo.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 
 public interface CorreoRepo extends CrudRepository<Correo, Integer> {
 
-    @Query("SELECT c FROM Correo c LEFT JOIN FETCH c.attachments WHERE c.emailId = :emailId")
-    Optional<Correo> findCorreoWithAttachments(@Param("emailId") int emailId);
+
 }
